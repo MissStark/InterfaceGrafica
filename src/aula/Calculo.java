4572b1907e17dac132d0d8315f7366f7a5c95c6b
Sarah Stark <sarah.1208@outlook.com>
2017-09-05 20:08:07 -0300

    Calculadora

---
+++ src/aula/Calculo.java
@@ -0,0 +1,25 @@
+package aula;
+
+public class Calculo {
+	public double soma(double v1, double v2){
+		double total;
+		total = v1 + v2;
+		return total;
+	}
+	public double sub(double v1, double v2){
+		double total;
+		total = v1 - v2;
+		return total;
+	}
+	public double mult(double v1, double v2){
+		double total;
+		total = v1 * v2;
+		return total;
+	}
+	public double div(double v1, double v2){
+		double total;
+		total = v1 / v2;
+		return total;
+	}
+	
+}
